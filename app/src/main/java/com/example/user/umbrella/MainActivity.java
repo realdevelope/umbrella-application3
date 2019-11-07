0ee16d9a0ce065fd216154e917a8357c7ac58a90
KCH6937 <48710060+KCH6937@users.noreply.github.com>
2019-11-07 17:04:21 +0900

    합친거

--- app/src/main/java/com/example/user/umbrella/MainActivity.java
+++ app/src/main/java/com/example/user/umbrella/MainActivity.java
@@ -1,17 +1,62 @@
 package com.example.user.umbrella;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button1;
+    private TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //button1 = findViewById(R.id.button);      // xml에 아이디 지정 후, 버튼 아이디 지정
+        //txtResult = findViewById(R.id.txtResult);     //  버튼 누를 시 나오는 위치값에 대한 텍스트 뷰 마찬가지로 아이디 지정
+
+        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= 23 &&
+                        ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                            0);
+                } else {
+                    Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    String provider = location.getProvider();
+                    double longitude = location.getLongitude();
+                    double latitude = location.getLatitude();
+                    double altitude = location.getAltitude();
+
+                    txtResult.setText(String.format("위치정보 : %s\n위도 : %s\n경도 : %s\n고도  : %s", provider, longitude, latitude, altitude));
+                    lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                            1000,
+                            1,
+                            gpsLocationListener);
+                    lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                            1000,
+                            1,
+                            gpsLocationListener);
+                }
+            }
+        });
 
         Handler hand = new Handler();
         hand.postDelayed(new Runnable() {
@@ -26,4 +71,32 @@ public class MainActivity extends AppCompatActivity {
             }
         }, 5000);
     }
+
+
+    final LocationListener gpsLocationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+
+            String provider = location.getProvider();
+            double longitude = location.getLongitude();
+            double latitude = location.getLatitude();
+            double altitude = location.getAltitude();
+
+
+            try {
+                txtResult.setText(String.format("위치정보 : %s\n위도 : %s\n경도 : %s\n고도  : %s", provider, longitude, latitude, altitude));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+    };
 }
